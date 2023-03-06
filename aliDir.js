d8456aa391654d89326209b8be1ff5a2c23dc3f8
tangjiangjie <hatname@163.com>
2023-03-06 22:36:55 +0800

    主程序脚本

--- aliDir.js
+++ aliDir.js
@@ -1,3 +1,15 @@
+//环境建立
+//npm install axios
+
+//命令行
+//node aliDir.js 目录深度0表示只列出第一级别999 应该能列出所有了  输入文件input.txt  输出文件outinfo.json
+//例如 刷新顶层文件夹
+//node aliDir.js 0 input.txt outinfo.json
+//例如 刷新全部目录
+//node aliDir.js 999 input.txt outinfo.json
+
+
+
 const axios = require('axios')
 var fs = require('fs');
 
@@ -151,12 +163,4 @@ async function main()
 	}
 	writefsync(args[2]||"outinfo.json",JSON.stringify(arr_files));
 }
-//环境建立
-//npm install axios
-
-//命令行
-//node aliDir.js 目录深度0表示只列出第一级别999 应该能列出所有了  输入文件input.txt  输出文件outinfo.json
-//例如
-
-//node aliDir.js 2 input.txt outinfo.json
 main()
